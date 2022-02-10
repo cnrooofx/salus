b58517dae4d2e32297985d3d72d11c62a753e6f9
alex-omeara <72926811+alex-omeara@users.noreply.github.com>
2022-02-10 13:35:40 +0000

    fixed login button not grabbing email value

--- client/preload.js
+++ client/preload.js
@@ -1,11 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const loginButton = document.getElementBy('login-form')
+    const loginButton = document.getElementById('login-form')
     loginButton.addEventListener('submit', login, 'false')
 
     function login(event) {
         event.preventDefault()
         const email = document.getElementById('email').value
         const loginText = document.getElementById('login-text')
+        console.log("hey")
         loginText.innerHTML = email
     }
 })
