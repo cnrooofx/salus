67e8718f8376411b67eaab804d8cd5f6bd7610f9
Aidan McCarthy <119333306@umail.ucc.ie>
2022-02-12 16:53:03 +0000

    Sign Up Working with Auto Increament

--- server/server.js
+++ server/server.js
@@ -1,29 +1,63 @@
 const express = require('express')
-const PASS = process.env.DB_PASS;
-console.log(PASS)
-var MongoClient = require('mongodb').MongoClient;
-var url = `mongodb+srv://test:${PASS}@solus-db-3a415e48.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=solus-db&tls=true&tlsCAFile=ca-certificate.crt`
+const { MongoClient, ServerApiVersion } = require("mongodb");
+var url = `mongodb+srv://test:7hH398r5cFQD46j0@solus-db-3a415e48.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=solus-db&tls=true&tlsCAFile=ca-certificate.crt`
+const client = new MongoClient(url, { serverApi: ServerApiVersion.v1 });
 const app = express()
 
 app.use(express.json())
 
+const hostname = '0.0.0.0';
+const port = 5000;
 const users = [{ name: 'Aidan'}]
+
+
 
 app.get('/users', (req, res) => {
     res.json(users)
 })
 
-app.post('/users',(req,res) => {
-    const uname = { name : req.body.name, password: req.body.password}
-    MongoClient.connect(url, function(err, db) {
+app.post('/signup',(req,res) => {
+  MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("users");
+      dbo.collection("counters").findOne({_id:"userid"}, function(err, result) {
         if (err) throw err;
-        var dbo = db.db("pwds");
-        dbo.collection("userp").insertOne(uname, function(err, res) {
-          if (err) throw err;
-          console.log("1 document inserted");
-          db.close();
-        });
-      });
-    res.status(201).send()
+            var myq = { _id:"userid" };
+            var newvalues = { $inc: { seq: 1 } };
+
+            dbo.collection("counters").updateOne(myq, newvalues, function(err, res) {
+              if (err) throw err;
+              console.log("1 document updated");
+    
+
+          const data = { _id: result.seq, uname : req.body.uname, password: req.body.pass}
+          dbo.collection("users").insertOne(data, function(err, res) {
+            if (err) throw err;
+  
+
+            console.log("User created");
+            db.close();
+          });
+    });
+    res.status(201).send();
+  });
 })
-app.listen(3000)
+})
+
+
+app.post('/users',(req,res) => {
+  const uname = { name : req.body.name, password: req.body.password}
+  MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("pwds");
+      dbo.collection("userp").insertOne(uname, function(err, res) {
+        if (err) throw err;
+        console.log("User created");
+        db.close();
+      });
+    });
+  res.status(201).send()
+})
+app.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}/`);
+});
