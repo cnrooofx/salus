5c01eeda3d9de43b3ea754d5d03140f414bd3ce2
Conor Fox <conor@conorfox.com>
2022-02-16 12:35:53 +0000

    Initialise & update password view from renderer

--- client/src/mainRenderer.js
+++ client/src/mainRenderer.js
@@ -20,14 +20,15 @@ var sidebarSelection = null
 if (Object.keys(accountData) == 0) {
     createListElement('empty', 'No Accounts Yet')
 } else {
+    initialisePasswordView()
+
     var idCounter = 0
     for (var account in accountData) {
         const listElement = createListElement('account' + idCounter, account)
-        listElement.addEventListener(onclick, event => {
-            console.log(event)
-        })
+        listElement.addEventListener('onclick', updatePasswordView)
         if (sidebarSelection == null) {
             listElement.setAttribute('class', 'active')
+            updatePasswordView(account)
             sidebarSelection = listElement
         }
         idCounter++
@@ -41,4 +42,69 @@ function createListElement(id, text) {
     listItem.appendChild(listText)
     sidebar.appendChild(listItem)
     return listItem
+}
+
+function changeSidebarSelection(newElement) {
+    sidebarSelection.setAttribute('class', '')
+    newElement.setAttribute('class', 'active')
+    sidebarSelection = newElement
+}
+
+function updatePasswordView(accountName) {
+    const username = accountData[accountName]['user']
+    const password = accountData[accountName]['pass']
+
+    const accountTitle = document.getElementById('accountTitle')
+    accountTitle.innerHTML = accountName
+
+    const usernameBox = document.getElementById('usernameBox')
+    usernameBox.setAttribute('value', username)
+
+    const passwordBox = document.getElementById('passwordBox')
+    passwordBox.setAttribute('value', password)
+}
+
+function initialisePasswordView() {
+    const passwordSection = document.getElementById('passwordView')
+
+    const title = document.createElement('h1')
+    const titleText = document.createTextNode('title')
+    title.setAttribute('id', 'accountTitle')
+    title.appendChild(titleText)
+    passwordSection.appendChild(title)
+
+    passwordSection.appendChild(document.createElement('hr'))
+
+    const usernameLabel = document.createElement('label')
+    const usernameLabelText = document.createTextNode('Username:')
+    usernameLabel.setAttribute('for', 'username')
+    usernameLabel.appendChild(usernameLabelText)
+    passwordSection.appendChild(usernameLabel)
+
+    const usernameBox = document.createElement('input')
+    usernameBox.setAttribute('name', 'username')
+    usernameBox.setAttribute('type', 'text')
+    usernameBox.setAttribute('value', 'username123')
+    usernameBox.setAttribute('id', 'usernameBox')
+    usernameBox.setAttribute('disabled', 'true')
+    passwordSection.appendChild(usernameBox)
+
+    const passwordLabel = document.createElement('label')
+    const passwordLabelText = document.createTextNode('Password:')
+    passwordLabel.setAttribute('for', 'password')
+    passwordLabel.appendChild(passwordLabelText)
+    passwordSection.appendChild(passwordLabel)
+
+    const passwordBox = document.createElement('input')
+    passwordBox.setAttribute('name', 'password')
+    passwordBox.setAttribute('type', 'password')
+    passwordBox.setAttribute('value', 'passwordpassword')
+    passwordBox.setAttribute('id', 'passwordBox')
+    passwordBox.setAttribute('disabled', 'true')
+    passwordSection.appendChild(passwordBox)
+
+    const editButton = document.createElement('button')
+    const editButtonText = document.createTextNode('Edit')
+    editButton.appendChild(editButtonText)
+    passwordSection.appendChild(editButton)
 }
