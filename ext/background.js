245df8618a00ac568bef966d12d38b9e9d59ff38
Aidan McCarthy <119333306@umail.ucc.ie>
2022-02-28 14:51:36 +0000

    refusing to work

--- ext/background.js
+++ ext/background.js
@@ -1,6 +1,16 @@
 let color = '#3aa757';
 
+chrome.action.setPopup({popup: "popup.html"})
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+function login_success() {
+  chrome.action.setPopup({popup: "main.html"});
+}
+
+function login_failure() {
+  chrome.action.setPopup({popup: "popup.html"});
+}
