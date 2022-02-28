300950f07255fdb2907b6782d8e945264f9cc05c
Aidan McCarthy <119333306@umail.ucc.ie>
2022-02-28 13:42:37 +0000

    Login JS Working

--- ext/popup.js
+++ ext/popup.js
@@ -1,19 +1,48 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+let loginBut = document.getElementById("loginBut");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  loginBut.style.backgroundColor = color;
 });
-
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+function changePopup(){
+    chrome.browserAction.setPopup({
+       popup:"main.html"
+    });
+}
+
+loginBut.addEventListener("click", async () => {
+
+    postData('http://salussecurity.live:5000/login', { "email":"aidan@u1cc.ie","pass": "fuwbfuwbfuwb"})
+  .then(data => {
+    if (data != false){
+        document.getElementById("p1").innerHTML = data // JSON data parsed by `data.json()` call
+        changePopup();
+    }
+    else {
+        document.getElementById("p1").innerHTML = "False"
+    }
   });
+    });
   
+
   // The body of this function will be executed as a content script inside the
   // current page
   function setPageBackgroundColor() {
